7d653ae60b11579b49a4a9435970702fa58c30ae
CamilleLec <lechevaliercamille@hotmail.fr>
2024-01-21 11:20:16 +0100

    avant modification li

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,6 +5,7 @@ const closeCarrousel = document.querySelector(".fa-xmark");
 const carrousel = document.querySelector(".carrousel");
 const container = document.querySelector(".container");
 let photographerMedia = [];
+let first;
 
 fetch("data/photographers.json")
     .then((response) => response.json())
@@ -16,29 +17,58 @@ fetch("data/photographers.json")
 
         const photographerFirstName = photographerData.name;
         const firstName = photographerFirstName.replace("-", "_").split(" ");
-        const first = firstName[0];
+        first = firstName[0];
 
         // Afficher les données de 'photographerData'
         if (photographerData) {
             displayData(photographerData);
         }
-        if (photographerMedia) {
-            const main = document.getElementById("main");
-            const div = document.createElement("div");
-            div.classList.add("medias");
-            main.appendChild(div);
-            photographerMedia.forEach((medias) => {
-                const factory = mediaFactory(medias, first);
-                const card = factory.getMediaCard();
-                div.appendChild(card);
-            });
-            closeCarrousel.addEventListener("click", (e) => {
-                carrousel.style.display = "none";
-                container.innerHTML = "";
 
+        renderCards();
 
-            });
+        // Fermer le carroussel
+        function closeLightbox() {
+            carrousel.style.display = "none";
         }
+        closeCarrousel.addEventListener("click", closeLightbox);
+
+        // Déplacement dans le carrousel
+        const right = document.getElementById("right");
+        const left = document.getElementById("left");
+
+        rightHandler = () => {
+            allPictures = document.querySelectorAll(".media-item");
+            currentIndex = (currentIndex + 1) % allPictures.length;
+            updateImage();
+        };
+
+        leftHandler = () => {
+            allPictures = document.querySelectorAll(".media-item");
+            currentIndex = (currentIndex - 1 + allPictures.length) % allPictures.length;
+            updateImage();
+        };
+        document.addEventListener("keydown", (e) => {
+            if (e.code === "ArrowLeft") leftHandler();
+            if (e.code === "ArrowRight") rightHandler();
+            if (e.code === "Escape") closeLightbox();
+        });
+
+        right.addEventListener("click", rightHandler);
+        left.addEventListener("click", leftHandler);
+
+        // Mise a jour de l'image/video dans le carrousel
+        function updateImage() {
+            if (allPictures[currentIndex].classList.contains("pictures")) {
+                newImg.src = allPictures[currentIndex].src;
+                newImg.style.display = "block";
+                newVid.style.display = "none";
+            } else {
+                newVid.src = allPictures[currentIndex].src;
+                newVid.style.display = "block";
+                newImg.style.display = "none";
+            }
+        }
+        // }
     });
 
 function displayData(photographerData) {
@@ -71,6 +101,46 @@ function displayData(photographerData) {
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     photographercontent.appendChild(img);
-
     button.after(img);
 }
+
+function renderCards() {
+    if (photographerMedia) {
+        const main = document.getElementById("main");
+        const div = document.getElementById("cards");
+
+        div.innerHTML = "";
+        photographerMedia.forEach((medias, index) => {
+            const factory = mediaFactory(medias, first);
+            const card = factory.getMediaCard();
+            card.setAttribute("tabindex", index + 10);
+            div.appendChild(card);
+        });
+    }
+}
+
+function comparelikes(a, b) {
+    return a.likes - b.likes;
+}
+
+setTimeout(() => {
+    photographerMedia.sort(comparelikes);
+    renderCards();
+}, 3000);
+
+const select = document.getElementById("orderBy");
+const ul = document.querySelector("ul");
+const chevronUp = document.querySelector(".fa-chevron-up");
+const chevronDown = document.querySelector(".fa-chevron-down");
+chevronDown.style.display = "none"
+select.addEventListener("click", () => {
+    if (chevronUp.style.display === "block"){
+    ul.style.display = "block";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";}
+    else {
+        ul.style.display = "none";
+        chevronUp.style.display = "block";
+        chevronDown.style.display = "none"
+    }
+});
--- scripts/utils/mediaFactory.js
+++ scripts/utils/mediaFactory.js
@@ -1,16 +1,27 @@
 let allPictures;
 let currentIndex = 0;
 let rightHandler, leftHandler;
+const newVid = document.querySelector(".carrousel video");
+const newImg = document.querySelector(".carrousel img");
 
 function mediaFactory(media, folder) {
     const first = folder;
     const { photographerId, title, image, video, likes, date, price, id } = media;
 
+    // const select = document.getElementById("orderBy");
+    // select.addEventListener("select", () => {
+    //     console.log("ok");
+    // });
+
+    function orderBy() {likes.sort()}
+
     function getMediaCard() {
         const img = document.createElement("img");
+        img.classList.add("media-item");
         const card = document.createElement("div");
         card.classList.add("card");
         const movie = document.createElement("video");
+        movie.classList.add("media-item");
         const textTitle = document.createElement("h3");
         textTitle.textContent = title;
         const like = document.createElement("span");
@@ -18,46 +29,31 @@ function mediaFactory(media, folder) {
         cardContent.classList.add("cardContent");
         like.innerHTML = `${likes} <i class="fa-solid fa-heart"></i>`;
 
+        card.addEventListener("click", () => {
+            carrousel.style.display = "block";
+
+            if (image) {
+                newImg.style.display = "block";
+                newImg.src = img.src;
+                allPictures = document.querySelectorAll(".media-item");
+                currentIndex = [...allPictures].indexOf(img);
+                newVid.style.display = "none";
+            } else if (video) {
+                newVid.style.display = "block";
+                newVid.src = movie.src;
+                newImg.style.display = "none";
+            }
+        });
+
         if (image) {
             const photographerImages = `assets/images/${first}/${image}`;
             img.src = photographerImages;
-            img.className = "pictures";
+            img.classList.add("pictures");
             card.appendChild(img);
             cardContent.appendChild(textTitle);
-
-            img.addEventListener("click", (e) => {
-                carrousel.style.display = "block";
-                const clonedImg = img.cloneNode(true);
-                clonedImg.className = "photographerElements";
-                allPictures = document.querySelectorAll(".pictures");
-                currentIndex = [...allPictures].indexOf(img);
-                container.appendChild(clonedImg);
-
-                const right = document.getElementById("right");
-                const left = document.getElementById("left");
-
-                rightHandler = () => {
-                    currentIndex = (currentIndex + 1) % allPictures.length;
-                    updateImage();
-                };
-
-                leftHandler = () => {
-                    currentIndex = (currentIndex - 1 + allPictures.length) % allPictures.length;
-                    updateImage();
-                };
-
-                right.addEventListener("click", rightHandler);
-                left.addEventListener("click", leftHandler);
-
-                function updateImage() {
-                    const imageActive = document.querySelector(".photographerElements");
-                    imageActive.src = allPictures[currentIndex].src;
-                }
-            });
         } else if (video) {
             const photographerVideo = `assets/images/${first}/${video}`;
             movie.src = photographerVideo;
-
             card.appendChild(movie);
             cardContent.appendChild(textTitle);
         }
